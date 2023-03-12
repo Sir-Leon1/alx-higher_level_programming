72a4896f186980ea1c5d25542d21a25ad7913d97
Gunnah Leon <eb3.61594.22@student.chuka.ac.ke>
2023-03-12 18:25:43 +0300

    Task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for j in range(97, 123):
-	if chr(j) not in ['q' , 'e']:
-		print("{}".format(chr(j)), end="")
+    if chr(j) not in ["q", "e"]:
+        print("{}".format(chr(j)), end="")
