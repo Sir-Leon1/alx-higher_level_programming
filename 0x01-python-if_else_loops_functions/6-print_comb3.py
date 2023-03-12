2b78b8ed218a07f4d7cf49b3757647ba911066ba
Gunnah Leon <eb3.61594.22@student.chuka.ac.ke>
2023-03-12 18:05:40 +0300

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for z in range(10):
     for x in range(z + 1, 10):
         if z == 8 and x == 9:
-            print(f"{z}{x}")
+            print("{}{}".format(z, x))
         else:
-            print(f"{z}{x}", end=", ")
+            print("{}{}".format(z, x), end=", ")
