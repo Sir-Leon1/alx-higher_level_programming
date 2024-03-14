fe551b61600d7171f0cd77e811b2cbeccd5a63ca
Sir-Leon1 <eb3.61594.22@student.chuka.ac.ke>
2024-03-14 14:51:43 +0300

    8-9

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Python is an iinterpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:113] + str[:6]
+print(str)
