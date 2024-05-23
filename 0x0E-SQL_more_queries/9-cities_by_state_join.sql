19da96a0c3855767a6370538c9b64871fe5829da
Sir-Leon <eb3.61594.22@student.chuka.ac.ke>
2024-05-23 17:53:29 +0300

    Tasks

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,3 @@
+-- Lista all cities in the DB hbtn_0d_usa
+-- Records are sorted in order of ascending cities.id
+SELECT c.`id`, c.`name`, s.`name` FROM `cities` AS c INNER JOIN `states` AS s ON c.`state_id` = s.`id` ORDER BY c.`id`;
