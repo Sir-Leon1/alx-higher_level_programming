48703352611af58ec68feacc5d71cf218846fdc7
Sir-Leon1 <eb3.61594.22@student.chuka.ac.ke>
2024-07-08 17:18:07 +0300

    Task 102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request to 0.0.0.0:5000/catch_ that gets message "You got me!"
-curl -sL -X PUT -H "Origin:" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -s 0.0.0.0:5000/catch_me -X GET -H "Origin: HolbertonSchool" -d "user_id=98" -o /dev/null -w "You got me!"
