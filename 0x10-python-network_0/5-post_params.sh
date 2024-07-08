9b7feb3faae12865b2d7c4ace345f60dc42966fa
Sir-Leon1 <eb3.61594.22@student.chuka.ac.ke>
2024-07-08 10:50:28 +0300

    Task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#Bash script that sends a POST request to the URL and display body
-#of the response
+#Bash script that sends a POST request to the URL and display response
 curl -s -X POST -d "email=test@gmail.com¥subject=I will always be here for PLD" "$1"
